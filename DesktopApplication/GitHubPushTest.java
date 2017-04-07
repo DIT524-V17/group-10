26dce54b46f2b462abe8b01c7381aef4aab6ab74
Susanne14 <suzan.alsamraai@gmail.com>
2017-04-08 01:18:50 +0200

    Commit a file to github to test, will remove it on next commit.

---
+++ DesktopApplication/GitHubPushTest.java
@@ -0,0 +1,9 @@
+/**
+ * Created by SuzanneElias on 2017-04-08.
+ */
+public class GitHubPushTest {
+
+    public static void main(String args[]) {
+        System.out.println("Hello World Push Test");
+    }
+}
