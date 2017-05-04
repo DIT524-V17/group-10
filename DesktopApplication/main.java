10d161cf57e2c36c208690a66ba52d4fcb5b9ff6
Ludvig <Ludvighygrell@gmail.com>
2017-05-04 13:18:44 +0200

    Added button for twitch

--- DesktopApplication/main.java
+++ DesktopApplication/main.java
@@ -4,8 +4,7 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
-
-
+import javax.bluetooth.BluetoothStateException;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 
@@ -13,28 +12,34 @@ import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.io.IOException;
+
 import javax.swing.JLabel;
 import java.awt.Color;
 import java.awt.Font;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
-public class main extends JFrame{
+public class main extends JFrame {
 
 	private JPanel contentPane;
 	private JButton btnCont = new JButton("Connect as controller");
 	private JLabel connectionPic = new JLabel("");
-	
+
 	private boolean menu1 = true;
 	private boolean menuCont = false;
-	
+	private boolean menuTwitch = false;
+
 	private boolean carForward = false;
 	private boolean carLeft = false;
 	private boolean carRight = false;
 	private boolean carBack = false;
-	
+
 	private final JButton btnMakeDummyConnection = new JButton("Make dummy connection");
 	private final JButton btnBack = new JButton("Back");
 	private final JButton btnDisconnect = new JButton("Disconnect");
 	private final JButton btnStart = new JButton("Start");
+	private final JButton btnTwitch = new JButton("Connect to Twitch.tv");
 
 	/**
 	 * Launch the application.
@@ -64,7 +69,7 @@ public class main extends JFrame{
 		setFocusable(true);
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
+
 		btnCont.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent arg0) {
@@ -75,26 +80,40 @@ public class main extends JFrame{
 		btnCont.setBounds(70, 341, 229, 96);
 		btnCont.setFocusable(false);
 		contentPane.add(btnCont);
-		
-		
+
 		connectionPic.setBounds(294, 16, 239, 53);
 		connectionPic.setIcon(new ImageIcon(main.class.getResource("/resource/notcon.png")));
 		connectionPic.setVisible(false);
 		connectionPic.setFocusable(false);
 		contentPane.add(connectionPic);
-		
+
 		btnMakeDummyConnection.addMouseListener(new MouseAdapter() {
 			@Override
-			public void mouseClicked(MouseEvent e) {
-				connectionPic.setIcon(new ImageIcon(main.class.getResource("/resource/conn.png")));
-				btnStart.setVisible(true);
+			public void mouseClicked(MouseEvent e){
+				Bluetooth bluetooth = new Bluetooth();
+				try {
+					bluetooth.findCar();
+				} catch (BluetoothStateException | InterruptedException e1) {
+					e1.printStackTrace();
+				}
+				if (bluetooth.getCarFound()) {
+					try {
+						bluetooth.connect();
+					} catch (IOException e1) {
+						e1.printStackTrace();
+					}
+					connectionPic.setIcon(new ImageIcon(main.class.getResource("/resource/conn.png")));
+					btnStart.setVisible(true);
+				} else {
+					System.out.println("Car not found");
+				}
 			}
 		});
 		btnMakeDummyConnection.setBounds(294, 78, 244, 39);
 		btnMakeDummyConnection.setFocusable(false);
 		btnMakeDummyConnection.setVisible(false);
 		contentPane.add(btnMakeDummyConnection);
-		
+
 		btnBack.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
@@ -118,7 +137,7 @@ public class main extends JFrame{
 		btnDisconnect.setFocusable(false);
 		btnDisconnect.setVisible(false);
 		contentPane.add(btnDisconnect);
-		
+
 		btnStart.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
@@ -131,23 +150,51 @@ public class main extends JFrame{
 		btnStart.setVisible(false);
 		btnStart.setBackground(Color.GREEN);
 		btnStart.setBounds(703, 216, 260, 145);
-		
+
 		contentPane.add(btnStart);
+		btnTwitch.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		
+
+		btnTwitch.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				menu1();
+				menuTwitch();
+			}
+		});
+		btnTwitch.setBounds(70, 478, 229, 96);
+		contentPane.add(btnTwitch);
 	}
-	public void menu1(){
-		if(menu1) menu1 = false;
-		else menu1 = true;
+
+	public void menu1() {
+		if (menu1)
+			menu1 = false;
+		else
+			menu1 = true;
 		btnCont.setVisible(menu1);
+		btnTwitch.setVisible(menu1);
 	}
-	public void menuCont(){
-		if(menuCont) menuCont= false;
-		else menuCont = true;
+
+	public void menuCont() {
+		if (menuCont)
+			menuCont = false;
+		else
+			menuCont = true;
 		connectionPic.setVisible(menuCont);
 		btnMakeDummyConnection.setVisible(menuCont);
 		btnBack.setVisible(menuCont);
 		btnDisconnect.setVisible(menuCont);
 	}
-	public void disconnectController(){
+	public void menuTwitch(){
+		if (menuTwitch)
+			menuTwitch = false;
+		else
+			menuTwitch = true;
+	}
+	public void disconnectController() {
 		connectionPic.setIcon(new ImageIcon(main.class.getResource("/resource/notcon.png")));
 		btnStart.setVisible(false);
 	}
