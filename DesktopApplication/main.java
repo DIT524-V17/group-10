4ac6ac29db3933749b8c1287080fe2880d8a2cd1
Ludvig <Ludvighygrell@gmail.com>
2017-05-05 11:35:44 +0200

    More new GUI stuff
    Added error messages and the option to "try again" after a connection failure

--- DesktopApplication/main.java
+++ DesktopApplication/main.java
@@ -18,10 +18,18 @@ public class main extends JFrame {
 	public static Bluetooth bluetooth;
 	
 	private JLabel lblSearching = new JLabel("");
+	private JLabel lblConnecting = new JLabel("");
 	private final JLabel lblController = new JLabel("");
 	private final JLabel lblTwitch = new JLabel("");
+	private JLabel lblTryAgain = new JLabel("");
+	private final JLabel lblNoCar = new JLabel("");
+	private final JLabel lblConnFail = new JLabel("");
+
 	private Sound clickSound;
 	private Sound mouseoverSound;
+	private Sound tryAgain;
+
+
 
 	/**
 	 * Launch the application.
@@ -45,7 +53,10 @@ public class main extends JFrame {
 	public main() {
 		clickSound = new Sound("/resource/clickSound.wav");
 		mouseoverSound = new Sound("/resource/mouseoverSound.wav");
+		tryAgain = new Sound("/resource/tryAgain.wav");
+		
 		setTitle("SMLR");
+		setFocusable(true);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 1000, 800);
 		contentPane = new JPanel();
@@ -58,15 +69,17 @@ public class main extends JFrame {
 		
 		lblSearching.setIcon(new ImageIcon(main.class.getResource("/resource/searching.png")));
 		lblSearching.setBounds(249, 146, 584, 373);
+		lblSearching.setVisible(false);
 		contentPane.add(lblSearching);
 		
-		JLabel lblConnecting = new JLabel("");
+		
 		lblConnecting.setVisible(false);
 		lblConnecting.setBounds(191, 96, 673, 528);
 		lblConnecting.setIcon(new ImageIcon(main.class.getResource("/resource/connecting.png")));
 		contentPane.add(lblConnecting);
 		
 		lblController.setBounds(285, 74, 476, 225);
+		lblController.setVisible(false);
 		lblController.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseEntered(MouseEvent arg0) {
@@ -91,7 +104,8 @@ public class main extends JFrame {
 		lblController.setIcon(new ImageIcon(main.class.getResource("/resource/controller.png")));
 		contentPane.add(lblController);
 		
-		
+		//CONNECT TO TWITCH BUTTON
+		lblTwitch.setVisible(false);
 		lblTwitch.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseEntered(MouseEvent e) {
@@ -113,22 +127,67 @@ public class main extends JFrame {
 		lblTwitch.setIcon(new ImageIcon(main.class.getResource("/resource/twitch.png")));
 		contentPane.add(lblTwitch);
 		
-		/**
+		//TRY AGAIN BUTTON
+		lblTryAgain.setVisible(false);
+		lblTryAgain.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				lblNoCar.setVisible(false);
+				lblConnFail.setVisible(false);
+				lblTryAgain.setVisible(false);
+				SearchAndConnect();
+				tryAgain.play();
+			}
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				mouseoverSound.play();
+				lblTryAgain.setBounds(257, 560, 430, 144);
+				lblTryAgain.setIcon(new ImageIcon(main.class.getResource("/resource/tryAgain2.png")));
+			}
+			@Override
+			public void mouseExited(MouseEvent e) {
+				lblTryAgain.setBounds(266, 563, 419, 134);
+				lblTryAgain.setIcon(new ImageIcon(main.class.getResource("/resource/tryAgain.png")));
+			}
+		});
+		lblTryAgain.setBounds(266, 563, 419, 134);
+		contentPane.add(lblTryAgain);
+		lblTryAgain.setIcon(new ImageIcon(main.class.getResource("/resource/tryAgain.png")));
+		
+		lblNoCar.setBounds(210, 96, 647, 481);
+		lblNoCar.setIcon(new ImageIcon(main.class.getResource("/resource/NoFind.png")));
+		contentPane.add(lblNoCar);
+		
+		lblConnFail.setVisible(false);
+		lblConnFail.setBounds(172, 98, 622, 512);
+		lblConnFail.setIcon(new ImageIcon(main.class.getResource("/resource/ConnFail.png")));
+		contentPane.add(lblConnFail);
+		
+		
+		SearchAndConnect();
+	}
+	public void SearchAndConnect(){
 		try {
+			lblSearching.setVisible(true);
 			bluetooth.findCar();
 			lblSearching.setVisible(false);
 		} catch (BluetoothStateException | InterruptedException e) {
-			e.printStackTrace();
+			lblNoCar.setVisible(true);
+			lblSearching.setVisible(false);
+			lblTryAgain.setVisible(true);
 		}
 		if(bluetooth.getCarFound()){
 			try {
 				lblConnecting.setVisible(true);
 				bluetooth.connect();
 				lblConnecting.setVisible(false);
+				lblController.setVisible(true);
+				lblTwitch.setVisible(true);
 			} catch (IOException e) {
-				e.printStackTrace();
+				lblConnFail.setVisible(true);
+				lblSearching.setVisible(false);
+				lblTryAgain.setVisible(true);
 			}
 		}
-		*/
 	}
 }
