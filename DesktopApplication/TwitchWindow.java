1e049b89fb15089f1f90a9f72159d852087c8bda
Ludvig <Ludvighygrell@gmail.com>
2017-05-24 21:41:07 +0200

    Twitch Window GUI added
    GUI prototype added. The images are temporary. The GUI requires testing

--- DesktopApplication/TwitchWindow.java
+++ DesktopApplication/TwitchWindow.java
@@ -4,10 +4,18 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
 
 public class TwitchWindow extends JFrame {
 
 	private JPanel contentPane;
+	
+	static JLabel forward;
+	static JLabel left;
+	static JLabel right;
+	static JLabel backward;
+	static JLabel nothing;
 
 	/**
 	 * Launch the application.
@@ -33,13 +41,62 @@ public class TwitchWindow extends JFrame {
 		setBounds(100, 100, 1600, 900);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		forward = new JLabel("");
+		forward.setVisible(false);
+		forward.setIcon(new ImageIcon(TwitchWindow.class.getResource("/resource/forward1.png")));
+		forward.setBounds(440, 328, 70, 91);
+		contentPane.add(forward);
+		
+		left = new JLabel("");
+		left.setVisible(false);
+		left.setIcon(new ImageIcon(TwitchWindow.class.getResource("/resource/left1.png")));
+		left.setBounds(251, 285, 147, 91);
+		contentPane.add(left);
+		
+		right = new JLabel("");
+		right.setVisible(false);
+		right.setIcon(new ImageIcon(TwitchWindow.class.getResource("/resource/right1.png")));
+		right.setBounds(260, 409, 159, 108);
+		contentPane.add(right);
+		
+		backward = new JLabel("");
+		backward.setVisible(false);
+		backward.setIcon(new ImageIcon(TwitchWindow.class.getResource("/resource/back1.png")));
+		backward.setBounds(373, 192, 116, 103);
+		contentPane.add(backward);
+		
+		nothing = new JLabel("");
+		nothing.setVisible(false);
+		nothing.setIcon(new ImageIcon(TwitchWindow.class.getResource("/resource/tryAgain.png")));
+		nothing.setBounds(405, 554, 614, 392);
+		contentPane.add(nothing);
 		
 		TwitchCommandSend test = new TwitchCommandSend();
 		String twitch = "C:/Users/Ludvig/AppData/Roaming/mIRC/logs/#riffslayerboy.Twitch.tv.log";
 		String testis = "C:/Users/Ludvig/Desktop/t/twitch.txt";
 		test.sendCommands(testis);
 	}
-
+	static void commandStart(DriveLog log){
+		if(log.getCmdtype().equals("@forward")){
+			forward.setVisible(true);
+		} else if (log.getCmdtype().equals("@left")){
+			left.setVisible(true);
+		} else if(log.getCmdtype().equals("@right")){
+			right.setVisible(true);
+		} else if(log.getCmdtype().equals("@backward")){
+			backward.setVisible(true);
+		} else {
+			nothing.setVisible(true);
+		}
+	}
+	static void commandEnd(JLabel lbl){
+		forward.setVisible(false);
+		left.setVisible(false);
+		right.setVisible(false);
+		backward.setVisible(false);
+		nothing.setVisible(false);
+	}
 }
