d6320403571ac72a5bd811f94fa56abda0c6dc12
Ludvig <Ludvighygrell@gmail.com>
2017-05-24 21:45:06 +0200

    Democracy revision
    Various changes for the sake of integration

--- DesktopApplication/democracy.java
+++ DesktopApplication/democracy.java
@@ -1,36 +1,49 @@
 import java.util.ArrayList;
 import java.util.Random;
 
-public class democracy {
-	private Random ran = new Random();
-	private String[] list;
-	private counter forwardCounter = new counter("@forward");
-	private counter rightCounter = new counter("@right");
-	private counter leftCounter = new counter("@left");
-	private counter backwardCounter = new counter("@backward");
-	private counter[] counters = new counter[4];
-	private ArrayList<counter> biggestCounters = new ArrayList<counter>(1);
+public class Democracy {
 
-	public democracy() {
+	
+	private Random ran = new Random();
+	private DriveLog forward = new DriveLog("@forward","TO",4000);
+	private DriveLog right = new DriveLog("@right","RI",500);
+	private DriveLog left = new DriveLog("@left","LE",600);
+	private DriveLog backward = new DriveLog("@backward","BO",3000);
+	private DriveLog stop = new DriveLog("@stop","TF",500);
+
+	private counter forwardCounter = new counter("@forward", forward);
+	private counter rightCounter = new counter("@right", right);
+	private counter leftCounter = new counter("@left", left);
+	private counter backwardCounter = new counter("@backward", backward);
+	private counter[] counters = new counter[4];
+	private ArrayList<counter> biggestCounters = new ArrayList<counter>();
+	
+	public ArrayList<counter> getCounters(){
+		return this.biggestCounters;
+	}
+
+	public Democracy() {
 		counters[0] = forwardCounter;
 		counters[1] = leftCounter;
 		counters[2] = rightCounter;
 		counters[3] = backwardCounter;
+
 	}
 
-	public String getResult(String[] list) {
-		this.list = list;
-		for (int i = 0; i < list.length; i++) {
-			if(list[i] != null){
-			if (list[i].equals("@forward")) {
-				forwardCounter.increment();
-			} else if (list[i].equals("@left")) {
-				leftCounter.increment();
-			} else if (list[i].equals("@right")) {
-				rightCounter.increment();
-			} else if (list[i].equals("@backward")) {
-				backwardCounter.increment();
-			}
+	public DriveLog getResult(Executor exec) {
+		clear();
+	    System.out.println("index is " + main.indexCounter);
+		for (int i = main.indexCounter ; i < exec.size(); i++) {
+			if (exec.getArray().get(i) != null) {
+				if (exec.getArray().get(i).getCmdtype().equals("@forward")) {
+					forwardCounter.increment();
+				} else if (exec.getArray().get(i).getCmdtype().equals("@left")) {
+					leftCounter.increment();
+				} else if (exec.getArray().get(i).getCmdtype().equals("@right")) {
+					rightCounter.increment();
+				} else if (exec.getArray().get(i).getCmdtype().equals("@backward")) {
+					backwardCounter.increment();
+				}
 			}
 		}
 		int highest = -1;
@@ -44,11 +57,42 @@ public class democracy {
 				biggestCounters.add(counters[i]);
 			}
 		}
-		if(biggestCounters.size() == 1){
-			return biggestCounters.get(0).getCommand();
+		//TESTING PRINTS
+		for (int i = 0; i < counters.length; i++) {
+			System.out.println("          " + counters[i].getCommand() + ": " + counters[i].getCounter());
+			
 		}
-		else{
-			return biggestCounters.get(ran.nextInt(biggestCounters.size())).getCommand();
+		for (int i = 0; i < biggestCounters.size(); i++) {
+			System.out.println("--------" + biggestCounters.get(i).getDriveLog().getCmdtype());
+		}
+		if (highest == 0) {
+			System.out.println("Something is being returned");
+			return stop;
+		} else if (biggestCounters.size() == 1) {
+			System.out.println("Something is being returned");
+			System.out.println("size: " + biggestCounters.size());
+			return biggestCounters.get(0).getDriveLog();
+			
+
+		} else {
+			System.out.println("Something is being returned");
+			return biggestCounters.get(ran.nextInt(biggestCounters.size())).getDriveLog();
 		}
 	}
+
+	public void clear() {
+		for (int i = 0; i < counters.length; i++) {
+			counters[i].reset();
+			System.out.println("          " + counters[i].getCommand() + ": " + counters[i].getCounter());
+			
+		}
+		biggestCounters.clear();
+		biggestCounters.trimToSize();
+		for (int i = 0; i < counters.length; i++) {
+
+		}
+		System.out.println("Size = " + biggestCounters.size());
+
+	}
+
 }
