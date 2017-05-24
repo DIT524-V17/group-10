f1adcbfb3119ea9655a053bc96272894ff8bd14a
Susanne14 <gussuzal@student.gu.se>
2017-05-25 00:01:52 +0200

    Update controller.java

--- DesktopApplication/controller.java
+++ DesktopApplication/controller.java
@@ -1,4 +1,6 @@
 import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.EventQueue;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
@@ -12,16 +14,21 @@ import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.awt.image.BufferedImage;
 import java.io.IOException;
 
 import javax.swing.JLabel;
 import javax.swing.border.BevelBorder;
 import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.GraphicsEnvironment;
 
 import uk.co.caprica.vlcj.binding.LibVlc;
 import uk.co.caprica.vlcj.runtime.RuntimeUtil;
 
 import com.sun.jna.NativeLibrary;
+
 import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -29,21 +36,31 @@ import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import javax.swing.JTextField;
 import javax.swing.SwingUtilities;
-import uk.co.caprica.vlcj.component.EmbeddedMediaPlayerComponent;
 
+import uk.co.caprica.vlcj.component.DirectMediaPlayerComponent;
+import uk.co.caprica.vlcj.component.EmbeddedMediaPlayerComponent;
+import uk.co.caprica.vlcj.discovery.NativeDiscovery;
+import uk.co.caprica.vlcj.player.direct.BufferFormat;
+import uk.co.caprica.vlcj.player.direct.BufferFormatCallback;
+import uk.co.caprica.vlcj.player.direct.DirectMediaPlayer;
+import uk.co.caprica.vlcj.player.direct.RenderCallback;
+import uk.co.caprica.vlcj.player.direct.RenderCallbackAdapter;
+import uk.co.caprica.vlcj.player.direct.format.RV32BufferFormat;
 import uk.co.caprica.vlcj.binding.LibVlc;
 import uk.co.caprica.vlcj.runtime.RuntimeUtil;
 
 import com.sun.jna.NativeLibrary;
 import uk.co.caprica.vlcj.component.EmbeddedMediaPlayerComponent;
-//Author: Ludvig Hygrell
+
 
 public class controller {
     
-        private static final String NATIVE_LIBRARY_SEARCH_PATH = "C:\\Users\\Gautam\\Documents\\vlc";
-        private static EmbeddedMediaPlayerComponent mediaPlayerComponent;
-        private final JFrame frame;
-	private JPanel contentPane;
+	private final JPanel mediaPanel;
+    private final BufferedImage img;
+    private static final int width = 352;
+    private static final int height = 288;
+    private final JFrame frame;
+    private final DirectMediaPlayerComponent mediaPlayerComponent;
 	
 	private boolean keyPress = false;
 	private boolean carForward = false;
@@ -90,25 +107,38 @@ public class controller {
 	 */
 	public controller() {
             
-            NativeLibrary.addSearchPath(RuntimeUtil.getLibVlcLibraryName(), NATIVE_LIBRARY_SEARCH_PATH);
-        System.out.println(LibVlc.INSTANCE.libvlc_get_version());
+		 boolean found = new NativeDiscovery().discover();
+         System.out.println(found);
+         System.out.println(LibVlc.INSTANCE.libvlc_get_version());
         
-        frame = new JFrame("jStreamPlayer");
+        frame = new JFrame("Controller");
         frame.setBounds(50, 100, 850, 600);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
        
+        JPanel basePanel = new JPanel();
+        basePanel.setLayout(new BorderLayout());
+        mediaPanel = new mediaSurface();
+        mediaPanel.setLayout(new BorderLayout());
         
-        
-        
-        JPanel mediaBase = new JPanel();
-        mediaBase.setLayout(new BorderLayout());
-
-        mediaPlayerComponent = new EmbeddedMediaPlayerComponent();
-        mediaBase.add(mediaPlayerComponent, BorderLayout.CENTER);
-
-        JPanel panel = new JPanel();
-        panel.setLayout(new BorderLayout());
- //      urlField = new JTextField("http://192.168.0.2:8008/test1.mpg",50);
+        basePanel.add(mediaPanel,BorderLayout.CENTER);
+        img = GraphicsEnvironment
+                .getLocalGraphicsEnvironment()
+                .getDefaultScreenDevice()
+                .getDefaultConfiguration()
+                .createCompatibleImage(width, height);
+            BufferFormatCallback bufferFormatCallback = new BufferFormatCallback() {
+                @Override
+                public BufferFormat getBufferFormat(int sourceWidth, int sourceHeight) {
+                    return new RV32BufferFormat(width, height);
+                }
+            };
+            
+            mediaPlayerComponent = new DirectMediaPlayerComponent(bufferFormatCallback)  {
+                @Override
+                protected RenderCallback onGetRenderCallback() {
+                    return new jPlayRenderCallback();
+                }
+            };
 
         JPanel speedPanel = new JPanel();
         JPanel directionPanel = new JPanel();
@@ -149,9 +179,9 @@ public class controller {
         panel.add(speedPanel,BorderLayout.NORTH);
         panel.add(directionPanel,BorderLayout.CENTER);
         
-       mediaBase.add(panel,BorderLayout.WEST);
-       frame.setBounds(50, 40, 640, 500);
-        frame.setContentPane(mediaBase);
+       basePanel.add(panel,BorderLayout.WEST);
+  
+        frame.setContentPane(basePanel);
         frame.setVisible(true);
        mediaPlayerComponent.getMediaPlayer().playMedia("http://192.168.0.1:8000/picam.webm");
               
@@ -167,50 +197,37 @@ public class controller {
 			}
 		});
 		
-//		contentPane = new JPanel();
-//		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-//		
-//		contentPane.setLayout(new BorderLayout() );
-//                panel.setBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null));
-//		//panel.setBounds(165, 54, 749, 497);
-//		
-//		contentPane.add(panel);
-//		panel.setLayout(null);
-//		//lblVideoFeed.setFont(new Font("Tahoma", Font.PLAIN, 43));
-//		//lblVideoFeed.setBounds(281, 185, 348, 101);
-//		panel.add(mediaPlayerComponent,BorderLayout.NORTH);
-//             //   panel.setVisible(true);
-//		
-//		//lblSpeed.setBounds(15, 54, 69, 20);
-//		contentPane.add(lblSpeed,BorderLayout.WEST);
-//		
-//
-//		//lblPrevention.setBounds(15, 89, 102, 20);
-//		contentPane.add(lblPrevention,BorderLayout.WEST);
-//		
-//
-//		//left.setBounds(15, 203, 50, 50);
-//		left.setIcon(new ImageIcon(main.class.getResource("/resource/left0.png")));
-//		contentPane.add(left,BorderLayout.WEST);
-//		
-//
-//		//right.setBounds(75, 203, 50, 50);
-//		right.setIcon(new ImageIcon(main.class.getResource("/resource/right0.png")));
-//		contentPane.add(right,BorderLayout.WEST);
-//		
-//		//forward.setBounds(44, 142, 50, 50);
-//		forward.setIcon(new ImageIcon(main.class.getResource("/resource/forward0.png")));
-//		contentPane.add(forward,BorderLayout.WEST);
-//		
-//		//back.setBounds(44, 264, 50, 50);
-//		back.setIcon(new ImageIcon(main.class.getResource("/resource/back0.png")));
-//		contentPane.add(back,BorderLayout.WEST);
-//		
-//		//frame.add(panel);
-//                frame.setContentPane(contentPane);
-//                frame.setVisible(true);
-//              mediaPlayerComponent.getMediaPlayer().playMedia("http://dl1.webmfiles.org/elephants-dream.webm");
 	}
+    
+    private class mediaSurface extends JPanel {
+
+        private mediaSurface() {
+            setBackground(Color.black);
+            setOpaque(true);
+            setPreferredSize(new Dimension(width, height));
+            setMinimumSize(new Dimension(width, height));
+            setMaximumSize(new Dimension(width, height));
+        }
+
+        @Override
+        protected void paintComponent(Graphics g) {
+            Graphics2D g2 = (Graphics2D)g;
+            g2.drawImage(img, null, 0, 0);
+        }
+    }
+    private class jPlayRenderCallback extends RenderCallbackAdapter {
+
+        private jPlayRenderCallback() {
+            super(new int[width * height]);
+        }
+
+        @Override
+        protected void onDisplay(DirectMediaPlayer mediaPlayer, int[] rgbBuffer) {
+            // Simply copy buffer to the image and repaint
+            img.setRGB(0, 0, width, height, rgbBuffer, 0, width);
+            mediaPanel.repaint();
+        }
+    }
 	public void processKeyEvent(KeyEvent e) {
 		if (e.getID() == KeyEvent.KEY_PRESSED) {
 			
